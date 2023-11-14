751f7f421fb0aa1b240178e2472995ac5c01d0eb
Shenglinggithub <150832910+Shenglinggithub@users.noreply.github.com>
2023-11-14 12:22:25 -0600

    Update github_model.R

--- github_model.R
+++ github_model.R
@@ -1,4 +1,4 @@
 model <- readRDS("VTE Risk Assessment Model for Lymphoma.rds")
-summary(model)
+#summary(model)
 your_data <- read.csv("sample_data.csv")
 your_data$predited_incidence<- predict(model, newdata =your_data, type="risk",times = 6)
