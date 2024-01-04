7ec2d49a96893b8eabe7d2a38d4a0c650551fe0f
Shenglinggithub <150832910+Shenglinggithub@users.noreply.github.com>
2024-01-04 13:23:51 -0600

    Add files via upload

--- github_model.R
+++ github_model.R
@@ -1,36 +1,42 @@
 library(survivalROC)
 library(riskRegression)
 # load your dataset
+setwd("/Users/shenglingma/Library/CloudStorage/Box-Box/CPRIT VTE Projects/HHS Database/Project/Lymphoma RAM/shiny/archive/Github")
 your_data <- read.csv("sample_data.csv")
 
 # load two Fine and Gray models
 model_VTE <- readRDS("VTE Risk Assessment Model for Lymphoma.rds")
 model_PEDVT <- readRDS("DVT Risk Assessment Model for Lymphoma.rds")
 
-# get predicted VTE or PE/LE-DVT risk for each patient at month "n"
-n <- 6 ## could adjust to any time you'd like to predict
-your_data$predited_VTE_incidence<- predict(model_VTE, newdata =your_data, type="risk",times = n)
-your_data$predited_PEDVT_incidence<- predict(model_PEDVT, newdata =your_data, type="risk",times = n)
 
-# prepare the binary outcomes for Time dependent AUC at month "n"
+
+# make sure "Lymphoma_histology" is factor type with c("0","1", "2","3") levels.
+your_data$Lymphoma_histology <-  factor(your_data$Lymphoma_histology, levels = c("0","1", "2","3"))
+# get predicted VTE or PE/LE-DVT risk for each patient at 6 month 
+your_data$predited_VTE_incidence<- predict(model_VTE, newdata =your_data, type="risk",times = 6)
+your_data$predited_PEDVT_incidence<- predict(model_PEDVT, newdata =your_data, type="risk",times = 6)
+
+# prepare the binary outcomes for Time dependent AUC
 your_data$VTE_indicator <- ifelse(your_data$status_VTE=="1",1,0)
 your_data$PEDVT_indicator <- ifelse(your_data$status_PEDVT=="1",1,0)
+unique(your_data$VTE_indicator)
 
-# TD_AUC using fgr predicted incidence as marker
+## Use when you want a TD-ROC value and 95% CI that is bootstrapped
 make_survival_roc <- function(resampled_data, time_col, status_col, marker_col){
   survivalROC(resampled_data[[time_col]], resampled_data[[status_col]],
-              marker = resampled_data[[marker_col]], predict.time = n, method = "NNE",
+              marker = resampled_data[[marker_col]], predict.time = 6, method = "NNE",
+              # span = 0.04*nobs^(-0.2)
               span = 0.0055)
 }
 
-# make_bootstrap_ci calls this function
+## make_bootstrap_ci calls this function
 make_bootstrap_auc <- function(data, indices, time_col, status_col, marker_col) {
   resampled_data <- data[indices,]
   out1 <- make_survival_roc(resampled_data, time_col, status_col, marker_col)
   out1$AUC
 }
 
-# Use when you want a TD-ROC value and 95% CI that is bootstrapped
+## Use when you want a TD-ROC value and 95% CI that is bootstrapped
 make_bootstrap_ci <- function(data, time_col, status_col, marker_col,
                               R, round_num = 3){
   boot_auc <- boot::boot(data = data, statistic = make_bootstrap_auc,
@@ -49,7 +55,9 @@ make_bootstrap_ci <- function(data, time_col, status_col, marker_col,
              Upper = upper,
              Str = glue::glue("{est} ({lower}-{upper})"))
 }
-
-R <-10 # could be 1000 if your data size is large enough
+str(your_data)
+R <-10 # could be 1000 if your sample size is large enough
 TD_AUC_VTE <- make_bootstrap_ci(your_data, "time_VTE_month","VTE_indicator","predited_VTE_incidence",R)
 TD_AUC_PEDVT <- make_bootstrap_ci(your_data, "time_PEDVT_month","PEDVT_indicator","predited_PEDVT_incidence",R)
+TD_AUC_VTE
+TD_AUC_PEDVT
